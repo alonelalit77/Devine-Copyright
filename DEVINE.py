bf1142a9c275024eef25b515b35deb6884239fa5
Devine <devineparadoxz@gmail.com>
2024-07-23 13:44:22 +0530

    Update DEVINE.py

--- DEVINE.py
+++ DEVINE.py
@@ -1,4 +1,8 @@
+from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
 
-DEVINE = Client("copyright_protector_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+# Initialize the bot client
+DEVINE = Client("main_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
+# Start the bot
+DEVINE.run()
