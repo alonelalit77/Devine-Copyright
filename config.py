58c91f8937afab1c6f94224b0e2ec3edf855b786
Devine <devineparadoxz@gmail.com>
2024-07-23 13:35:43 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+
+API_ID = 21264701
+API_HASH = "6c053fa8e2e7f5cd1699e4501ef13f52"
+BOT_TOKEN = "6924632975:AAFSdjX8sbwA90L0TfmsXKU6GKbaHdazp7A"
+MONGO_DB_URL = "mongodb+srv://devineparadoxz:wAn0pVWsvgsmWh6n@cluster0.4ajkyn4.mongodb.net/?retryWrites=true&w=majority"
+OWNER_ID = 6096082088
